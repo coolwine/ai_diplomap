50fa486f8a9be626efd6dab3dfb0d97c9759cb3d
du <du@ncsoft.com>
2026-04-16 09:08:06 +0900

    Working...

--- src/data/leaders.ts
+++ src/data/leaders.ts
@@ -25,4 +25,99 @@ export const LEADERS: Record<string, LeaderInfo> = {
     title: "President (2013–present)",
     image: "/leaders/cn.jpg",
   },
+  CA: {
+    name: "Mark Carney",
+    title: "24th Prime Minister (2025–present)",
+    image: "/leaders/ca.jpg",
+  },
+  GB: {
+    name: "Keir Starmer",
+    title: "Prime Minister (2024–present)",
+    image: "/leaders/gb.jpg",
+  },
+  IL: {
+    name: "בנימין נתניהו (Benjamin Netanyahu)",
+    title: "Prime Minister (2022–present)",
+    image: "/leaders/il.jpg",
+  },
+  IN: {
+    name: "नरेन्द्र मोदी (Narendra Modi)",
+    title: "Prime Minister (2014–present)",
+    image: "/leaders/in.jpg",
+  },
+  IR: {
+    name: "مجتبی خامنه‌ای (Mojtaba Khamenei)",
+    title: "Supreme Leader (2026–present)",
+    image: "/leaders/ir.jpg",
+  },
+  KP: {
+    name: "김정은 (Kim Jong Un)",
+    title: "Supreme Leader (2011–present)",
+    image: "/leaders/kp.jpg",
+  },
+  MX: {
+    name: "Claudia Sheinbaum",
+    title: "President (2024–present)",
+    image: "/leaders/mx.jpg",
+  },
+  PH: {
+    name: "Ferdinand Marcos Jr.",
+    title: "17th President (2022–present)",
+    image: "/leaders/ph.jpg",
+  },
+  RU: {
+    name: "Владимир Путин (Vladimir Putin)",
+    title: "President (2012–present)",
+    image: "/leaders/ru.jpg",
+  },
+  SA: {
+    name: "محمد بن سلمان (Mohammed bin Salman)",
+    title: "Crown Prince & Prime Minister (2022–present)",
+    image: "/leaders/sa.jpg",
+  },
+  TW: {
+    name: "賴清德 (Lai Ching-te)",
+    title: "President (2024–present)",
+    image: "/leaders/tw.jpg",
+  },
+  UA: {
+    name: "Володимир Зеленський (Volodymyr Zelenskyy)",
+    title: "President (2019–present)",
+    image: "/leaders/ua.jpg",
+  },
+  BR: {
+    name: "Luiz Inácio Lula da Silva",
+    title: "39th President (2023–present)",
+    image: "/leaders/br.jpg",
+  },
+  EG: {
+    name: "عبد الفتاح السيسي (Abdel Fattah el-Sisi)",
+    title: "President (2014–present)",
+    image: "/leaders/eg.jpg",
+  },
+  ID: {
+    name: "Prabowo Subianto",
+    title: "8th President (2024–present)",
+    image: "/leaders/id.jpg",
+  },
+  NP: {
+    name: "Balendra Shah",
+    title: "43rd Prime Minister (2026–present)",
+    image: "/leaders/np.png",
+  },
+  PL: {
+    name: "Karol Nawrocki",
+    title: "President (2025–present)",
+    image: "/leaders/pl.jpg",
+  },
+  TR: {
+    name: "Recep Tayyip Erdoğan",
+    title: "12th President (2014–present)",
+    image: "/leaders/tr.jpg",
+  },
+  VN: {
+    name: "Tô Lâm",
+    title: "General Secretary (2024–present)",
+    image: "/leaders/vn.jpg",
+  },
 };
